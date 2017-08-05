e9c61c02bf3f9109efbca85733a9b6510369265f
かなざわ <lcac.p1.izumo@gmail.com>
2017-08-05 12:01:59 +0900

    Command_.java追加

---
+++ LCAC-P1-IZUMO/app/src/main/java/DataBase/Command.java
@@ -0,0 +1,32 @@
+package DataBase;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.util.Log;
+
+/**
+ * Created by admin_m on 2017/08/05.
+ */
+
+public class Command {
+
+    /**
+     * テーブルを作成する
+     * @param db SQLiteのインスタンス
+     */
+    public static void sqlCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL("DROP TABLE IF EXISTS Command");
+            db.execSQL("CREATE TABLE Command ("
+                    + " ScenarioId      INTEGER primary key autoincrement"
+                    + ",GroupeId        INTEGER "
+                    + ",ScenarioName    TEXT"
+                    + ");"
+            );
+        }
+        catch (SQLiteException ex) {
+            Log.e("SQLiteException", ex.getMessage());
+        }
+    }
+
+}
