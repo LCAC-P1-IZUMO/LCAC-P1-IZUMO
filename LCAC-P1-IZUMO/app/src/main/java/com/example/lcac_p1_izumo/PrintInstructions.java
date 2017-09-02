f6bf2d4cf6aeeaabd57c0e4a453329711c406a12
Takeshi <lcac.p1.izumo@gmail.com>
2017-09-02 11:17:54 +0900

    イベント画面、グループ画面遷移追加

--- LCAC-P1-IZUMO/app/src/main/java/com/example/lcac_p1_izumo/PrintInstructions.java
+++ LCAC-P1-IZUMO/app/src/main/java/com/example/lcac_p1_izumo/PrintInstructions.java
@@ -1,6 +1,7 @@
 package com.example.lcac_p1_izumo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,15 @@ public class PrintInstructions extends Activity {
     //**************************//
     //****** コントロール ******//
     //**************************//
-    /**
-     * [前画面に戻る]ボタン
-     */
+
+    /** [前画面に戻る]ボタン */
     private Button mBtnReturn;
+
+    /** [グループ編集]ボタン */
+    private Button m_btnGroupEdit;
+
+    /** [イベント編集]ボタン */
+    private Button m_btnEventEdit;
 
     //**************************//
     //**** イベントリスナー ****//
@@ -29,6 +35,28 @@ public class PrintInstructions extends Activity {
         public void onClick(View v) {
             // 閉じる
             finishAndRemoveTask();
+        }
+    };
+    /**
+     * [グループ編集]ボタンを押下した時の処理を定義
+     */
+    private Button.OnClickListener onClick_btnGroupEdit = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // [グループ編集]を開く
+            Intent intent = new Intent(getApplication(), GroupCreationScreen.class);
+            startActivity(intent);
+        }
+    };
+    /**
+     * [イベント編集]ボタンを押下した時の処理を定義
+     */
+    private Button.OnClickListener onClick_btnEventEdit = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // [イベント編集]を開く
+            Intent intent = new Intent(getApplication(), ScenarioListActivity.class);
+            startActivity(intent);
         }
     };
 
@@ -45,5 +73,20 @@ public class PrintInstructions extends Activity {
 
         // [前画面に戻る]ボタンを押下した時の処理を設定
         mBtnReturn.setOnClickListener(mOnClick_btnReturn);
+
+        // [グループ編集]ボタン
+        m_btnGroupEdit = (Button)findViewById(R.id.btnGroupEdit);
+        m_btnGroupEdit.setOnClickListener(onClick_btnGroupEdit);
+
+        // [イベント編集]ボタン
+        m_btnEventEdit = (Button)findViewById(R.id.btnEventEdit);
+        m_btnEventEdit.setOnClickListener(onClick_btnEventEdit);
     }
+
+
+
+
+
+
+
 }
